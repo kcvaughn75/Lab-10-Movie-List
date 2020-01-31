a850cd59c9d15477a39e59003d45e07fc63f4b51
Kyle Vaughn <kcvaughn75@gmail.com>
2020-01-31 09:04:57 -0500

    first submit for Lab 10

---
+++ Lab 10 Movie List/Movie.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_Movie_List
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        private static List<string> GetListOfMovieCategories(List<Movie> movieList)
+        {     //creating object\/
+            List<string> movieCategory = new List<string>();
+            foreach (Movie movie in movieList)
+            {
+                if (!movieCategory.Contains(movie.Category))
+                {
+                    movieCategory.Add(movie.Category);
+                }
+            }
+            return movieCategory;
+        }
+
+        public static void PrintMovieCategories(List<Movie> movieList)
+        {
+            List<string> movieCategories = GetListOfMovieCategories(movieList);
+            foreach (string category in movieCategories)
+            {
+                Console.WriteLine($"{category}, ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintMoviesByCategory(List<Movie> movieList, string categorySelected)
+        {
+            foreach (Movie movie in movieList)
+            {
+                if (categorySelected == movie.category)
+                {
+                    if (movie.category == "scifi")
+                    {
+                        Console.WriteLine($"{movie.title}");
+                    }
+                    if (movie.category == "action")
+                    {
+                        Console.WriteLine($"{movie.title}");
+                    }
+                    if (movie.category == "comedy")
+                    {
+                        Console.WriteLine($"{movie.title}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lab 10 Movie List/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> grandCircusVideo = new List<Movie>
+            {
+                new Movie("inception", "scifi"),
+                new Movie("the martian", "scifi"),
+                new Movie("interstellar", "scifi"),
+                new Movie("step brothers", "comedy"),
+                new Movie("iron man", "action"),
+                new Movie("dumb and dumber", "comedy"),
+                new Movie("superbad", "comedy"),
+                new Movie("die hard", "action"),
+                new Movie("the terminator", "action"),
+                new Movie("the matrix", "scifi")
+
+            };
+            char userContinue = 'y';
+            while (userContinue == 'y')
+            {
+                Console.WriteLine("Here is a list of movie categories to choose from: ");
+                Movie.PrintMovieCategories(grandCircusVideo);
+
+                Console.WriteLine("Please enter a movie category to see a list of movies:");
+                string categorySelected = Console.ReadLine().ToLower();
+
+                Movie.PrintMoviesByCategory(grandCircusVideo, categorySelected);
+
+                Console.WriteLine("Would you like to choose another category? [y/n]");
+                userContinue = char.Parse(Console.ReadLine().ToLower());
+
+                if (userContinue != 'y' && userContinue != 'n')
+                {
+                    Console.WriteLine("Invalid input. Please enter [y] to search for another movie, [n] to quit. [y/n]");
+                    userContinue = char.Parse(Console.ReadLine().ToLower());
+                }
+
+                if (userContinue == 'n')
+                {
+                    Console.WriteLine("Thanks for using Grand Circus Video Online! Goodbye!!");
+                    break;
+                }
+            }
+            
+
+
+            
+        }
+    }
+}
